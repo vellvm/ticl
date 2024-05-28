c55506503bbf4983ac3e58ec3b99827bed9e2f10
Nicolas Chappe <nicolas.chappe@ens-lyon.fr>
2024-05-28 20:56:33 +0200

    Fix extraction

--- tests/ImpBrTest/extracted/Extract.v
+++ tests/ImpBrTest/extracted/Extract.v
@@ -1,11 +1,12 @@
 Require Extraction.
+Require ExtrOcamlBasic.
 Require ExtrOcamlIntConv.
+Unset Universe Checking.
 Require ImpBrEx.ImpBrEx.
 
 Extraction Language OCaml.
 Extraction Blacklist String List Char Core Z.
 
-Cd "extracted".
+Recursive Extraction Library ExtrOcamlBasic.
 Recursive Extraction Library ExtrOcamlIntConv.
 Recursive Extraction Library ImpBrEx.
-Cd "..".
--- tests/ImpBrTest/main.ml
+++ tests/ImpBrTest/main.ml
@@ -1,36 +1,48 @@
 open CTreeDefinitions;;
 open ExtrOcamlIntConv;;
 open ImpBrEx;;
-open Datatypes;;
 open Printf;;
 
 let choose2 () =
-  if Random.int 2 = 1 then Coq_true else Coq_false;;
+  if Random.int 2 = 1 then true else false;;
 
-let rec run t =
+let rec run1 t =
   match observe t with
   | RetF r -> print_int (int_of_nat r)
   | VisF (_e, _k) -> failwith "Vis (unreachable)"
-  | BrF (_b, c, k) ->
-    match c with
-    | (* B0 *) Coq_inl _ -> failwith "Stuck"
-    | (* B1 *) Coq_inr (Coq_inl _) -> run (k (Obj.magic Coq_tt))
-    | (* B2 *) Coq_inr (Coq_inr _) -> run (k (Obj.magic choose2()));;
+  | BrF (_, k) -> run1 (k (Obj.magic choose2()))
+  | GuardF t -> run1 t
+  | StuckF -> failwith "Stuck"
+  | _ -> failwith "unreachable";;
+
+let rec run t =
+  match observe t with
+  | RetF r -> print_int (int_of_nat r); true
+  | VisF (_e, _k) -> failwith "Vis (unreachable)"
+  | BrF (_, k) ->
+    let b = choose2() in
+    if run (k (Obj.magic b)) then true else run (k (Obj.magic (not b)))
+  | GuardF t -> run t
+  | StuckF -> false
+  | _ -> failwith "unreachable";;
 
 let rec collect t =
   match observe t with
   | RetF r -> [int_of_nat r]
   | VisF (_e, _k) -> failwith "Vis (unreachable)"
-  | BrF (_b, c, k) ->
-    match c with
-    | (* B0 *) Coq_inl _ -> []
-    | (* B1 *) Coq_inr (Coq_inl _) -> collect (k (Obj.magic Coq_tt))
-    | (* B2 *) Coq_inr (Coq_inr _) -> collect (k (Obj.magic Coq_true)) @ collect (k (Obj.magic Coq_false));;
+  | GuardF t -> collect t
+  | StuckF -> []
+  | BrF (_, k) ->
+    collect (k (Obj.magic true)) @ collect (k (Obj.magic false))
+  | _ -> failwith "unreachable";;
 
 Random.self_init ();;
 print_string "Possible return values:";;
 let results = collect (Obj.magic ex_prog');;
 List.iter (printf " %d") results;;
+print_string "\nRandom execution (naïve): ";;
+try run1 (Obj.magic ex_prog') with
+| _ -> print_string "stuck";;
 print_string "\nRandom execution: ";;
 run (Obj.magic ex_prog');;
 print_newline ()
